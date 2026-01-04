a549858686974b0e328fa051c22616b35c982030
shani gamage <shaninehansa716@gmail.com>
2026-01-04 21:59:17 +0530

    updates

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Shani Gamage Portfolio</title>
+      <meta name="description" content="Portfolio website of Shani Gamage" />
+      <link rel="icon" href="/favicon.ico" /> 
+    </>
+  );
+}
